176f9d58f6c0d091df72a39c079a8eee6381fa63
DerrickLegacy <ahaabwe.derrick67@gmail.com>
2026-03-27 14:14:07 +0300

    fix: varying report and dashboard income values ++++

--- resources/views/auth/splash.blade.php
+++ resources/views/auth/splash.blade.php
@@ -36,20 +36,15 @@
         }
     }
 </style>
+
 <body class="h-screen flex flex-col items-center justify-center bg-gray-50 text-gray-900">
-    <div class="relative w-full max-w-xl mb-4 px-4 ">
-        <svg viewBox="0 0 450 430" class="w-full h-auto">
-            <path
-                id="motion-path"
-                d="M 215 49 C 132 78 163 204 251 147 C 287 91 221 48 195 96 C 181 157 271 126 234 104"
-                fill="transparent"
-                stroke="#8200DB"
-                stroke-width="0"
-                stroke-linecap="round"
-                stroke-dasharray="o"
-                stroke-dashoffset="0" />
-        </svg>
-        <div id="moving-box" class="absolute top-0 left-0 w-6 h-7 md:w-14 md:h-14 bg-emerald-400 rounded-xl"></div>
+    <!-- Header -->
+    <div class="flex items-center justify-between h-20 sm:h-24 lg:h-32 px-4 sm:px-4 lg:px-8 mt-2">
+        <!-- Logo -->
+        <a class="block" href="{{ route('dashboard') }}">
+            <img class="object-cover object-center w-16 h-16 sm:w-20 sm:h-20 lg:w-24 lg:h-24 rounded-full border-2 border-purple-600"
+                src="{{ asset('images/nailville_logo_100x100.jpg') }}" alt="Authentication image" />
+        </a>
     </div>
 
     <h1 class="text-2xl sm:text-3xl md:text-4xl font-bold text-[#8200DB] text-center mb-2">Nailville Salon Nalya</h1>
@@ -63,8 +58,7 @@
         <span class="dot w-3 h-3 sm:w-4 sm:h-4 bg-[#D90082] rounded-full"></span>
         <span class="dot w-3 h-3 sm:w-4 sm:h-4 bg-[#00DB82] rounded-full"></span>
     </div>
-
-    <p class="text-sm sm:text-base text-gray-500">Loading...</p>
+    <p>Loading ...</p>
 
     <script>
         setTimeout(() => {
